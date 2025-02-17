35f909b1cfd5f2f5249522c5a1a2013b15161248
Affgs <agnegaizutyte05@gmail.com>
2025-02-17 13:50:47 +0200

    Pirmas darbas

--- 1. Masyvai/main.cpp
+++ 1. Masyvai/main.cpp
@@ -3,9 +3,25 @@
 using namespace std;
 
 int main() {
-    cout << "iveskite norimus elementus.\n";
+    int masyvas[100];
+    int n;
+    int size_st;
+
+cout << "Iveskite norimus elementus.\n";
+cin >> n;
+    for (int i = 0; i < n; i++) {
+        cin >> masyvas[i];
+        cout << masyvas[i] << " ";
+    }
+
+    for (int i=0; i<size_st; i++) {
+        for (int j=0; j<n; j++) {
+            swap(masyvas[i], masyvas[j]);
+        }
+    }
     return 0;
 }
+// ----------------------------------------------------------------------------------------- 2 ---------------------------------------------------------------------------------------------
 
 // TIP See CLion help at <a
 // href="https://www.jetbrains.com/help/clion/">jetbrains.com/help/clion/</a>.
