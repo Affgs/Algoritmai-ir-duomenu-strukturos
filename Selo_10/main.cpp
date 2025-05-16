0436ebbf6fab865c23e2eadfbd0736c5fb35cb95
Affgs <agnegaizutyte05@gmail.com>
2025-05-17 02:57:00 +0300

    selo 10

---
+++ Selo_10/main.cpp
@@ -0,0 +1,39 @@
+#include <iostream>
+#include <ctime>
+#include <vector>
+#include <algorithm>
+#include <iomanip>
+
+void shellSort(std::vector<int>& arr) {
+    int n = arr.size();
+    for (int gap = n/2; gap > 0; gap /= 2) {
+        for (int i = gap; i < n; i++) {
+            int temp = arr[i];
+            int j;
+            for (j = i; j >= gap && arr[j-gap] > temp; j -= gap) {
+                arr[j] = arr[j-gap];
+            }
+            arr[j] = temp;
+        }
+    }
+}
+
+int main() {
+    const int SIZE = 100000;
+    std::vector<int> arr(SIZE);
+    std::srand(std::time(0));
+
+    for (int i = 0; i < SIZE; i++) {
+        arr[i] = std::rand() % 1000000;
+    }
+
+    clock_t start = clock();
+    shellSort(arr);
+    clock_t end = clock();
+
+    double time = double(end-start)/CLOCKS_PER_SEC;
+    std::cout << std::fixed << std::setprecision(6);
+    std::cout << "Shell Sort su 100000 elementu: " << time << " sek.\n";
+
+    return 0;
+}
