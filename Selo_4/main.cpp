ff753ac66127fb79f521275bc0da8c7cfa5ac27a
Affgs <agnegaizutyte05@gmail.com>
2025-05-17 01:27:53 +0300

    selo 4

---
+++ Selo_4/main.cpp
@@ -0,0 +1,41 @@
+#include <iostream>
+#include <ctime>
+#include <cstdlib>
+#include <algorithm>
+#include <iomanip>
+
+using namespace std;
+
+void shellSort(int arr[], int n) {
+    for (int gap = n / 2; gap > 0; gap /= 2) {
+        for (int i = gap; i < n; i++) {
+            int temp = arr[i];
+            int j;
+            for (j = i; j >= gap && arr[j - gap] > temp; j -= gap) {
+                arr[j] = arr[j - gap];
+            }
+            arr[j] = temp;
+        }
+    }
+}
+
+int main() {
+    const int SIZE = 10000;
+    int arr[SIZE];
+    srand(time(0));
+
+    // Generuojame atsitiktinius duomenis
+    for (int i = 0; i < SIZE; i++) {
+        arr[i] = rand() % 100000;
+    }
+
+    clock_t start = clock();
+    shellSort(arr, SIZE);
+    clock_t end = clock();
+
+    double time = double(end - start) / CLOCKS_PER_SEC;
+    cout << fixed << setprecision(6);
+    cout << "Shell Sort su 10000 nesurikiuotu duomenu: " << time << " sek.\n";
+
+    return 0;
+}
