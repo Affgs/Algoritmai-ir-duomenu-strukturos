c5cc6ee881f423937b36a460bb72aea1d37ee423
Affgs <agnegaizutyte05@gmail.com>
2025-05-17 00:54:37 +0300

    burbulo 3

---
+++ Burbulo_3/main.cpp
@@ -0,0 +1,40 @@
+#include <iostream>
+#include <ctime>
+#include <cstdlib>
+#include <algorithm>
+#include <iomanip>
+
+using namespace std;
+
+void bubbleSort(int arr[], int n) {
+    for (int i = 0; i < n - 1; i++) {
+        bool swapped = false;
+        for (int j = 0; j < n - i - 1; j++) {
+            if (arr[j] > arr[j + 1]) {
+                swap(arr[j], arr[j + 1]);
+                swapped = true;
+            }
+        }
+        if (!swapped) break;
+    }
+}
+
+int main() {
+    const int SIZE = 5000;
+    int arr[SIZE];
+
+    // Generuojame jau surikiuotus duomenis
+    for (int i = 0; i < SIZE; i++) {
+        arr[i] = i;
+    }
+
+    clock_t start = clock();
+    bubbleSort(arr, SIZE);
+    clock_t end = clock();
+
+    double time = double(end - start) / CLOCKS_PER_SEC;
+    cout << fixed << setprecision(6);
+    cout << "Bubble Sort su jau surikiuotais duomenimis: " << time << " sek.\n";
+
+    return 0;
+}
